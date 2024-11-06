02f81674d9293a55e1dd7e4aa7e3dfbfd0bbbf91
mk06005@gmail.com <mk065005@gmail.com>
2024-11-06 21:38:37 +0530

    Adding - comments

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -18,7 +18,8 @@ const Clients = () => {
           // remove bg-white dark:bg-black dark:bg-grid-white/[0.05], h-[40rem] to 30rem , md:h-[30rem] are for the responsive design
           //   justify-center
           className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center  relative overflow-hidden"
-        > */}
+        > */} 
+        {/* //comments */}
         <InfiniteMovingCards
           items={testimonials}
           direction="right"
