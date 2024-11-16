dfa02767a41f277c178448cb5be3e2bb0535f657
mk06005@gmail.com <mk065005@gmail.com>
2024-11-16 22:06:55 +0530

    Removing - Link comp

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,13 +24,13 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <Link href="mailto:mk065005@gmail.com">
+        <a href="mailto:mk065005@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
             position="right"
           />
-        </Link>
+        </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
