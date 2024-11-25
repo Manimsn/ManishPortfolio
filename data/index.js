0b37b3a22c48215c17bb3ccff4fd30e4407ca6b3
mk06005@gmail.com <mk065005@gmail.com>
2024-11-25 17:31:54 +0530

    Rephrasing experience section

--- data/index.js
+++ data/index.js
@@ -178,29 +178,29 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Full Stack Developer at Transcentra FTS Pvt. Ltd",
+      desc: "Developed UI components and integrated APIs with React.js and Redux, enhancing user experience, performance, and adhering to Git and Agile practices.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      title: "Senior Full Stack Developer at Repco Home Finance India Pvt. Ltd",
+      desc: "Designed and developed web app using React, Express, SQL Server, Sequelize.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Senior Front-End Developer at Bi-Sol Pvt. Ltd. (Client: AT & T)",
+      desc: "Led a development team for a Micro-Front-End project for AT&T, focusing on building reusable components and integrating them with Sanity Headless CMS.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "Team Lead - Full Stack Developer at Bounteous Private Limited",
+      desc: "Designed Solution Design Documentation (SDD), led code reviews, developed reusable mobile components, and integrated Oracle Push Notifications into mobile applications.",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
